3f4a19cd9f299de24510a9360544f458f93962a1
Christian Kischy Haege <kischy@gmx.de>
2021-01-09 18:38:42 +0100

    Switched to brute force approach

--- p24-euler-Tests/LexicographicPermutationsTests.cs
+++ p24-euler-Tests/LexicographicPermutationsTests.cs
@@ -1,93 +1,85 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using p24_euler;
+using System;
 using System.Collections.Generic;
+
+
 
 namespace p24_euler_Tests
 {
     [TestClass]
     public class LexicographicPermutationsTests
     {
-        Permutations permu = new Permutations();
 
 
         [TestMethod]
-        public void TestGetNextPermutation1()
+        public void TestGetNextPermutation3Chars()
         {
-            List<char> current = new List<char>() { '0', '1', '2' };
-            List<char> expected = new List<char>() { '0', '2', '1' };
+            Permutations permu = new Permutations(new List<char>() { '2', '1', '0' });
 
-            CollectionAssert.AreEqual(expected, permu.GetNextPermutation(current));
+            String expected = "012";
+            Assert.AreEqual(expected, permu.GetPermutation(0));
+
+            expected = "021";
+            Assert.AreEqual(expected, permu.GetPermutation(1));
+
+            expected = "102";
+            Assert.AreEqual(expected, permu.GetPermutation(2));
+          
+
+        }
+
+        /*
+        [TestMethod]
+        public void TestGetNextPermutation4Chars()
+        {
+            Permutations permu = new Permutations(new List<char>() { '2', '1', '0', '3' });
+
+            List<char> expected = new List<char>() { '0', '1', '2', '3' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '1', '3', '2' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '2', '1', '3' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '2', '3', '1' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '3', '2', '1' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '1', '0', '2', '3' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
         }
 
         [TestMethod]
-        public void TestGetNextPermutation2()
+        public void TestGetNextPermutation5Chars()
         {
-            List<char> current = new List<char>() { '0', '2', '1' };
-            List<char> expected = new List<char>() { '1', '0', '2' };
+            Permutations permu = new Permutations(new List<char>() { '2', '1', '0', '3', '4' });
 
-            CollectionAssert.AreEqual(expected, permu.GetNextPermutation(current));
+            List<char> expected = new List<char>() { '0', '1', '2', '3', '4' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '1', '2', '4', '3' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '1', '3', '2', '4' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '1', '3', '4', '2' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '1', '4', '2', '3' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
+            expected = new List<char>() { '0', '1', '4', '3', '2' };
+            CollectionAssert.AreEqual(expected, permu.GetNextPermutation());
+
         }
+        */
 
-        [TestMethod]
-        public void TestGetNextPermutation3()
-        {
-            List<char> current = new List<char>() { '0', '1', '2', '3' };
-            List<char> expected = new List<char>() { '0', '1', '3', '2' };
-
-            CollectionAssert.AreEqual(expected, permu.GetNextPermutation(current));
-        }
-
-        [TestMethod]
-        public void TestGetNextPermutation4()
-        {
-            List<char> current = new List<char>() { '0', '1', '3', '2' };
-            List<char> expected = new List<char>() { '0', '2', '1', '3' };
-
-            CollectionAssert.AreEqual(expected, permu.GetNextPermutation(current));
-        }
-
-        [TestMethod]
-        public void TestGetNextPermutation5()
-        {
-            List<char> current = new List<char>() { '0', '2', '1', '3' };
-            List<char> expected = new List<char>() { '0', '2', '3', '1' };
-
-            CollectionAssert.AreEqual(expected, permu.GetNextPermutation(current));
-        }
-
-        [TestMethod]
-        public void TestGetNextPermutation6()
-        {
-            List<char> current = new List<char>() { '0', '2', '3', '1' };
-            List<char> expected = new List<char>() { '0', '3', '2', '1' };
-            List<char> actual = permu.GetNextPermutation(current);
-
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void TestGetNextPermutation7()
-        {
-            List<char> current = new List<char>() { '0', '3', '2', '1' };
-            List<char> expected = new List<char>() { '1', '0', '2', '3' };
-
-            List<char> actual = permu.GetNextPermutation(current);
-
-
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-
-        [TestMethod]
-        public void TestGetNextPermutation7()
-        {
-            List<char> current = new List<char>() { '0', '1', '2', '3', '4' };
-            List<char> expected = new List<char>() { '0', '1', '2', '4',, '3' };
-
-            List<char> actual = permu.GetNextPermutation(current);
-
-
-            CollectionAssert.AreEqual(expected, actual);
-        }
     }
 }
--- p24-euler/Permutations.cs
+++ p24-euler/Permutations.cs
@@ -6,143 +6,64 @@ namespace p24_euler
 {
     public class Permutations
     {
-        
-        private struct SimpleSwitchingAdPermutationIndexes
-        {
-            // A Simple switching permutation is one where only the characters of two ajacent indizies have two be switched to get the next permutation
-            public int firstInd;
-            public int secInd;
-            public bool iSSimpleSwitchingAjPermutation;
-        }
 
-        private static void SwapListElements(List<char> list,  int firstInd, int secInd)
-        {
-            char tmp = list[firstInd];
-            list[firstInd] = list[secInd];
-            list[secInd] = tmp;
-        }
+        List<String> permutations;
+        String chars;
 
 
-        private bool SecondLastCharLowerThanLastChar(List<char> perm)
+        public Permutations(List<char> characters)
         {
-            return perm[perm.Count - 2] < perm[perm.Count - 1];
-        }
+            characters.Sort();
+            chars = new String(characters.ToArray());
+            permutations = new List<String>();
 
-        private void SwapLastTwoChars(List<char> perm)
-        {
-            SwapListElements(perm, perm.Count - 2, perm.Count - 1);
+            CalcAllPermutations(chars, 0);
+            permutations.Sort();
         }
 
 
-        private SimpleSwitchingAdPermutationIndexes NextPermuationIsCreatedBySwitchingTwoNumbers(List<char> perm)
+        private void CalcAllPermutations(String str, int lInd)
         {
-
-            SimpleSwitchingAdPermutationIndexes ssi;
-            ssi.iSSimpleSwitchingAjPermutation = false;
-            ssi.firstInd = -1;
-            ssi.secInd = -1;
-
-            //Special case if the last two numbers are just switchable
-            if (SecondLastCharLowerThanLastChar(perm))
+            if (lInd == str.Length-1)
             {
-                ssi.firstInd = perm.Count - 2;
-                ssi.secInd = perm.Count - 1;
-                ssi.iSSimpleSwitchingAjPermutation = true;
-                return ssi;
+                permutations.Add(str);
             }
-
-
-            char lastChar = perm[perm.Count - 2];
-
-            for (int i = perm.Count - 3; i >= 1; --i) //Do not use the first number
+            else
             {
-                if(lastChar-1 == perm[i]) //They must be only 1 lexiographic value from each other, otherwise they can't be just switched
+                for (int i = lInd; i < str.Length; i++)
                 {
-                    ssi.firstInd = i;
-                    ssi.secInd = i+1;
-                    ssi.iSSimpleSwitchingAjPermutation = true;
-                    return ssi;
-                }
-                else
-                {
-                    lastChar = perm[i];
-                }
-            }                         
-
-            return ssi;
-        }
-
-
-        private char GetSmallestChar(List<char> perm)
-        {
-            char smChar = perm[0];
-            for(int i = 1; i < perm.Count; ++i)
-            {
-                if(smChar > perm[i])
-                {
-                    smChar = perm[i];
+                    if(lInd == i)
+                    {
+                        CalcAllPermutations(str, lInd + 1);
+                    }
+                    else
+                    {
+                        str = SwapCharacters(str, lInd, i);
+                        CalcAllPermutations(str, lInd + 1);
+                        str = SwapCharacters(str, lInd, i);
+                    }
                 }
             }
-
-            return smChar;
         }
 
-        private char GetNextSmallerCharReverse(char c, List<char> perm)
+        public static String SwapCharacters(String str, int i, int j)
         {
-            for(int i = perm.Count-1; i >=0 ; --i)
-            {
-                if(perm[i] < c)
-                {
-                    return perm[i];
-                }
-            }
+            char[] charArray = str.ToCharArray();
 
-            return c;
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+
+            return new string(charArray);
         }
 
-        private List<char> GetNewPermutation(List<char> current, int startLength, char AfterStartChar1, char AfterStartChar2, int endingStartInd, int endingLength)
+
+        public String GetPermutation(int index)
         {
-            List<char> newPerm = current;
-
-            List<char> startPerm = newPerm.GetRange(0, startLength);
-            List<char> endPerm = newPerm.GetRange(endingStartInd, endingLength);
-
-            newPerm = startPerm;
-            newPerm.Add(AfterStartChar1);
-            newPerm.Add(AfterStartChar2);
-
-            endPerm.Sort();
-
-            newPerm.AddRange(endPerm);
-
-
-            return newPerm;
+            return permutations[index];
         }
 
 
-        public List<char> GetNextPermutation(List<char> current)
-        {
-            SimpleSwitchingAdPermutationIndexes ssi = NextPermuationIsCreatedBySwitchingTwoNumbers(current);
 
-            if (ssi.iSSimpleSwitchingAjPermutation)
-            {
-                SwapListElements(current, ssi.firstInd, ssi.secInd);
-                return current;                
-            }
-
-            char smallestCharOfLastTwo = GetSmallestChar(current.GetRange(current.Count-2,2));
-            int smallestCharOfLastTwoIndex = current.IndexOf(smallestCharOfLastTwo);
-
-            char nextSmallerCharFromReverse = GetNextSmallerCharReverse(smallestCharOfLastTwo, current);
-            int nextSmallerCharFromReverseIndex = current.IndexOf(nextSmallerCharFromReverse);
-
-
-            List<char> newPerm = GetNewPermutation(current,
-                nextSmallerCharFromReverseIndex, smallestCharOfLastTwo, nextSmallerCharFromReverse,
-                nextSmallerCharFromReverseIndex + 1, smallestCharOfLastTwoIndex - (nextSmallerCharFromReverseIndex + 1));
-
-
-            return newPerm;
-        }
     }
 }
